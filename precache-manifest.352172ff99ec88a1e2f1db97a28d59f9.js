530732371d6b0a0b7b5c238923695ae911016a41
Vrokn <61293690+Vrokn@users.noreply.github.com>
2020-07-29 20:33:24 -0500

    Updates

---
+++ precache-manifest.352172ff99ec88a1e2f1db97a28d59f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f554a1fba66ef43af771daafc6fe9a39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "568dfe8fa4f96ca2d9fe",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3ff36c5e9b57d0416c5c",
+    "url": "/static/js/2.4209276b.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.4209276b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "568dfe8fa4f96ca2d9fe",
+    "url": "/static/js/main.b64c282c.chunk.js"
+  },
+  {
+    "revision": "3a47ded0670808e15bb5",
+    "url": "/static/js/runtime-main.fdb8a3ad.js"
+  }
+]);
